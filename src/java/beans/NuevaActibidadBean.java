d29b77eb7b39a397531f565f349360c92d2fe9c4
rodrigoRagna <rodrigo.rivera@ciencias.unam.mx>
2016-04-21 10:52:42 -0500

    coreccion del dao y el ben

---
+++ src/java/beans/NuevaActibidadBean.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import modelo.sql.Area;
+import modelo.sql.Profesor;
+import modelo.sql.Tipo;
+
+
+/**
+ *
+ * @author Rodrigo
+ */
+@Named(value = "actividadBean")
+@ViewScoped
+@ManagedBean
+public class NuevaActibidadBean implements Serializable {
+    
+    private int idActividad;
+     private Area area;
+     private Profesor profesor;
+     private Tipo tipo;
+     private int ICupodisponible;
+     private int ICupomaximo;
+     private String SNombre;
+     private String SDescripciom;
+     private Date FFechamodificacion;
+     private Date FFechadescripion;
+     
+     
+    public NuevaActibidadBean() {
+        
+    }
+
+    public void setIdActividad(int idActividad) {
+        this.idActividad = idActividad;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setICupodisponible(int ICupodisponible) {
+        this.ICupodisponible = ICupodisponible;
+    }
+
+    public void setICupomaximo(int ICupomaximo) {
+        this.ICupomaximo = ICupomaximo;
+    }
+
+    public void setSNombre(String SNombre) {
+        this.SNombre = SNombre;
+    }
+
+    public void setSDescripciom(String SDescripciom) {
+        this.SDescripciom = SDescripciom;
+    }
+
+    public void setFFechamodificacion(Date FFechamodificacion) {
+        this.FFechamodificacion = FFechamodificacion;
+    }
+
+    public void setFFechadescripion(Date FFechadescripion) {
+        this.FFechadescripion = FFechadescripion;
+    }
+    
+    public void guardarActividad (){
+    modelo.dao.ActividadDao.introducirActividad(idActividad, profesor, tipo, ICupodisponible, ICupomaximo, SNombre, SDescripciom, FFechamodificacion, FFechadescripion);
+    }
+
+    public int getIdActividad() {
+        return idActividad;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public int getICupodisponible() {
+        return ICupodisponible;
+    }
+
+    public int getICupomaximo() {
+        return ICupomaximo;
+    }
+
+    public String getSNombre() {
+        return SNombre;
+    }
+
+    public String getSDescripciom() {
+        return SDescripciom;
+    }
+
+    public Date getFFechamodificacion() {
+        return FFechamodificacion;
+    }
+
+    public Date getFFechadescripion() {
+        return FFechadescripion;
+    }
+    
+    
+}
--- src/java/modelo/dao/ActividadDao.java
+++ src/java/modelo/dao/ActividadDao.java
@@ -5,9 +5,16 @@
  */
 package modelo.dao;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import modelo.sql.Actividad;
+import modelo.sql.Profesor;
+import modelo.sql.Tipo;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.jboss.logging.Logger;
+
 
 /**
  *
@@ -15,36 +22,46 @@ import org.hibernate.Transaction;
  */
 public class ActividadDao {
     
-    Session session ;
-    Transaction tx;
-    Integer idActividad;
+      
+   public List<Actividad> getListaActividad(){
+    List<Actividad> listaActividad = new ArrayList<Actividad>();
+    Session session = ConfHibernate.getSessionFactory().getCurrentSession();
+       try {
+           session.beginTransaction();
+           Criteria criteria = session.createCriteria(Actividad.class);
+           listaActividad = criteria.list();
+           session.getTransaction().commit();
+       } catch (Exception e) {
+           
+       }
+       return listaActividad;
+   }
+   
+   public static void introducirActividad(int idActividad ,Profesor id_profesor ,Tipo id_tipo,
+                        int cupoDisponible, int cupoMaximo, String nombre, String descripcion
+                            ,Date fechaModificacion, Date fechaDescripcion){
+       Session session = ConfHibernate.getSessionFactory().getCurrentSession();
+       // crea el obeto activdad vacio
+       Actividad miActividad = new Actividad();
+       //llena todos los campos
+       miActividad.setIdActividad(idActividad);
+       miActividad.setProfesor(id_profesor);
+       miActividad.setTipo(id_tipo);
+       miActividad.setICupodisponible(cupoDisponible);
+       miActividad.setICupomaximo(cupoMaximo);
+       miActividad.setSNombre(nombre);
+       miActividad.setSDescripciom(descripcion);
+       miActividad.setFFechamodificacion(fechaModificacion);
+       miActividad.setFFechamodificacion(fechaModificacion);
+       
+       try {
+           session.beginTransaction();
+           session.save(miActividad);
+           session.getTransaction().commit();
+       } catch (Exception e) {
+           
+       }
     
-    private void init(){
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        tx = session.beginTransaction();
-    }
-    
-    private void rollback(){
-    if(tx != null){
-        tx.rollback();
-    
-    }
-    
-    }
-    
-   public Integer insert(Actividad obj){
-        Integer b = -1;
-        try{
-            init();
-            idActividad = (Integer) session.save(obj);
-            tx.commit();
-            b = idActividad;
-        }catch(Exception e){
-            rollback();
-        
-            
-        }
-        return b;
-    }
+   }
     
 }
