7a18ce590c647215353a575c81a7c4e85905e96e
rodrigoRagna <rodrigo.rivera@ciencias.unam.mx>
2016-04-20 21:43:16 -0500

    implemetacion de la vista

--- tlacuachesoft.sql
+++ tlacuachesoft.sql
@@ -24,16 +24,56 @@ CREATE TABLE PROFESOR(
 	CONSTRAINT "profesor_uRFC" UNIQUE (s_RFC),
 	CONSTRAINT "profesor_uCorreo" UNIQUE (s_correo)
 );
+
+---consulta de profesor
+select * from profesor;
+--- insertar de profesor
+--- empiezan en el 
+insert into profesor (id_Profesor, s_RFC,s_nombre,s_appaterno,s_apmaterno,s_contrasenha,s_correo)values
+ (1,'AER681A814','rodrigo','rivera','paz','12345','rodrigo@ciencias');
+insert into profesor (id_Profesor, s_RFC,s_nombre,s_appaterno,s_apmaterno,s_contrasenha,s_correo)values
+ (2,'AERdwadwa','rodrigo','rivera','paz','12345','valeria@ciencias');
+
+insert into profesor (id_Profesor,s_RFC,s_nombre,s_appaterno,s_apmaterno,s_contrasenha,s_correo)values
+ (3,'AE5dwadwa','rodrigo','rivera','paz','12345','santiga@ciencias');
+
+
+
 CREATE TABLE TIPO(
 	id_Tipo SERIAL,
 	s_Tipo character varying(32) NOT NULL,
 	CONSTRAINT "tipo_pk" PRIMARY KEY (id_Tipo)
 );
+
+--- consulta de tipo
+select * from Tipo;
+-- insserssiones
+-- empiezan en el 1
+insert into tipo (id_Tipo,s_Tipo)values(1,'Actividad al Apoyo Docencia');
+insert into tipo (id_Tipo,s_Tipo)values(2,'Actividad al Apoyo Divulgacion');
+insert into tipo (id_Tipo,s_Tipo)values(3,'Actividad al Apoyo Investigacion');
+
+
 CREATE TABLE AREA(
 	id_Area SERIAL,
 	s_Area character varying(32) NOT NULL,
 	CONSTRAINT "area_pk" PRIMARY KEY (id_Area)
 );
+
+--- consultad ede area
+select * from area;
+--insertar de area nunca va a cambiar  
+--- empieza desde el 3
+insert into area (id_Area,s_Area)values (1,'Computo Cientifico');
+insert into area (id_Area,s_Area)values (2,'Redes Computadora');
+insert into area (id_Area,s_Area)values (3,'Ingenieria Software Base Datos');
+insert into area (id_Area,s_Area)values (4,'Inteligencia Artificial');
+insert into area (id_Area,s_Area)values (5,'Imagenes Ambientes Virtuales');
+insert into area (id_Area,s_Area)values (6,'Teoria Computacion');
+
+
+
+
 CREATE TABLE ACTIVIDAD(
 	id_Actividad SERIAL,
 	id_Profesor integer NOT NULL,
@@ -56,6 +96,14 @@ CREATE TABLE ACTIVIDAD(
 		REFERENCES AREA (id_Area) MATCH SIMPLE
 		ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+--- consulta
+select * from actividad; 
+---- insertar
+insert into ACTIVIDAD(id_Profesor, id_Tipo,id_Area, i_CupoDisponible , i_CupoMaximo,s_Nombre,s_Descripciom,f_fechaModificacion, f_fechaDescripion)values 
+			(1,2,5,0,6,'Analiss de algorimos','nesesitamos a gente interesada en ','2016/06/28','2016/07/15');
+
+			
 CREATE TABLE SOLICITUD(
 	id_Solicitud SERIAL,
 	id_Actividad integer NOT NULL,
